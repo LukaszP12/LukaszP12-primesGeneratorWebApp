05c2aaf21d52e0428694131fbdb88472bd520c83
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-07-27 21:41:11 +0200

    prime numbers generator as Utils Class

---
+++ src/main/java/primeGeneratorWeb/utils/PrimeGenerator.java
@@ -0,0 +1,30 @@
+package primeGeneratorWeb.utils;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PrimeGenerator {
+
+    public static Set<Integer> primes(int num) {
+
+        Set<Integer> primes = new TreeSet<>();
+        primes.add(2);
+        primes.add(3);
+        primes.add(5);
+
+        for (int i = 5; i <= num; i = i + 2) {
+
+            if (!isDivPrime(primes, i)) {
+                primes.add(i);
+            }
+
+        }
+
+        return primes;
+    }
+
+    private static boolean isDivPrime(Set<Integer> primes, int i) {
+        return primes.stream().filter(x -> i % x == 0).findAny().isPresent();
+    }
+
+}
