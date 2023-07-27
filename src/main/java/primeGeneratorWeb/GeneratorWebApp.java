d1abd23aed85bd5559629bbd5e26b3bda11088f0
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-07-27 21:47:17 +0200

    spring boot starter for running the app

---
+++ src/main/java/primeGeneratorWeb/GeneratorWebApp.java
@@ -0,0 +1,14 @@
+package primeGeneratorWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GeneratorWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorWebApp.class, args);
+    }
+
+}
