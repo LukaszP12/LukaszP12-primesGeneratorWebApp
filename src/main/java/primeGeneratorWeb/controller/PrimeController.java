3723c7a1b593630c5ab50d4f34273d44fbe19f88
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-07-27 21:44:58 +0200

    Controller class with corresponding endpoints

---
+++ src/main/java/primeGeneratorWeb/controller/PrimeController.java
@@ -0,0 +1,39 @@
+package primeGeneratorWeb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import primeGeneratorWeb.service.PrimeServiceImpl;
+
+import java.util.Set;
+
+@Controller
+public class PrimeController {
+
+    @Autowired
+    PrimeServiceImpl primeServiceImpl;
+
+    @RequestMapping("/primes")
+    public String showPrimes(@RequestParam(value = "pNumber", required = false) Integer pNumber) {
+        return "primesForm";
+    }
+
+    @RequestMapping(value = "/getPrimes", method = RequestMethod.GET)
+    public String getPrimes(Model model, @RequestParam("pNumber") Integer pNumber) {
+        Set<Integer> primes = primeServiceImpl.getPrimes(pNumber);
+        model.addAttribute("primes", primes);
+
+        return "primesForm";
+    }
+
+    @RequestMapping(value = "/resetPrimes", method = RequestMethod.GET)
+    public String resetPrimes(Model model) {
+        model.addAttribute("primes", null);
+
+        return "primesForm";
+    }
+
+}
